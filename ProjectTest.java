36199944ad966e75214a7fda2f78f874f4cc786c
German Bernal <ger.e.bernal@gmail.com>
2019-06-20 08:56:02 -0500

    Add files via upload

---
+++ ProjectTest.java
@@ -0,0 +1,16 @@
+
+
+public class ProjectTest {
+    public static void main(String[] args) {
+
+   Project project = new Project();
+
+        project.setName("Website Project");
+        project.setDescription("Development of a website for a new real estate agency");
+        project.setInitialCost(400);
+
+        project.elevatorPitch();
+
+
+    }
+}
